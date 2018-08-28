59ba139536e57b6c98acc988c6d481d436ebea12
Jonathan Ramírez <jona16121995@gmail.com>
2018-08-27 18:56:30 -0600

    Update video service
    this update serves to not repeat the code

--- tubekids-client/src/app/services/video.service.ts
+++ tubekids-client/src/app/services/video.service.ts
@@ -11,21 +11,21 @@ export class VideoService {
 
   constructor(private http: HttpClient) { }
 
-  baseUrl: string = 'http://localhost:3000';
+  baseUrl: string = 'http://localhost:3000/api/videos';
 
   addVideo(newVideo: video, token: string){
-    return this.http.post<video>(`${this.baseUrl}/api/videos`, newVideo,{headers: {Authorization: token}}).map(res => res);
+    return this.http.post<video>(`${this.baseUrl}`, newVideo,{headers: {Authorization: token}}).map(res => res);
   }
 
   getVideos(token: string){
-    return this.http.get<video[]>(`${this.baseUrl}/api/videos`, {headers: {Authorization: token}}).map(res => res);
+    return this.http.get<video[]>(`${this.baseUrl}`, {headers: {Authorization: token}}).map(res => res);
   }
 
   editVideo(newVideo: video, id: string, token: string){
-    return this.http.put<video>(`${this.baseUrl}/api/videos` + id, newVideo, {headers: {Authorization: token}}).map(res => res);
+    return this.http.put<video>(`${this.baseUrl}` + id, newVideo, {headers: {Authorization: token}}).map(res => res);
   }
 
   deleteVideo(id: string, token: string){
-    return this.http.delete<video>(`${this.baseUrl}/api/videos` + id, {headers: {Authorization: token}}).map(res => res);
+    return this.http.delete<video>(`${this.baseUrl}` + id, {headers: {Authorization: token}}).map(res => res);
   }
 }
